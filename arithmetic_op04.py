c0d753efcfbfcae361c70d9743d2b45ec19f77e6
Akbar Abdullayev <abdullayevakbar0101@gmail.com>
2021-11-05 21:27:16 +0500

    init

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -3,7 +3,7 @@ number=523
 #Find the 'number' first digit and assign to x1.
 x1=number//100
 #Find the 'number' second digit and assign to x2.
-x2=(number%100)//10
+x2=(number//10)%10
 #Find the 'number' third digit and assign to x3.
 x3=number%10
 #Create a variable called 'answer' and assign it the sum of the three digits.
